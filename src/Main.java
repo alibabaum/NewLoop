a8f2b88fed335e86decaa6ff42618927869a9633
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-09-22 16:14:06 -0600

    Fixed one bottle

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //This is so I can call back to the original entered number.
         int beer = origBeerNum;
 
-        while (beer >= 1 ){
+        while (beer > 1 ){
             // had to change to 1 instead of zero so it would trigger the else statement
             System.out.println(beer + " bottles of beer on the wall. " + beer
                     + " bottles of beer. ");
@@ -21,10 +21,13 @@ public class Main {
                 System.out.println("Take one down, pass it around, " + beer + " bottles of beer on the wall." + "\n");
             }
         }
-        System.out.println(beer + " bottles of beer on the wall. " + beer + " bottles of beer.");
-        System.out.println("Go to the store, buy some more. " + origBeerNum + " bottles of beer on the wall.");
+        if (beer ==1){
+            System.out.println(beer + " bottle of beer on the wall. " + beer + " bottle of beer.");
+            System.out.println("Go to the store, buy some more. " + origBeerNum + " bottles of beer on the wall.");
 
-
-
+        } else {
+            System.out.println(beer + " bottles of beer on the wall. " + beer + " bottles of beer.");
+            System.out.println("Go to the store, buy some more. " + origBeerNum + " bottles of beer on the wall.");
+        }
     }
 }
