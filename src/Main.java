44ca63bade16dfaeaa7f67c0ad4affbcc6589058
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-09-21 18:35:07 -0600

    With comments and print breaks

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,21 @@ public class Main {
         Scanner intScanner = new Scanner(System.in);
         System.out.println("How many bottles of beer?");
         int origBeerNum = intScanner.nextInt();
+        //This is so I can call back to the original entered number.
         int beer = origBeerNum;
 
         while (beer >= 1 ){
+            // had to change to 1 instead of zero so it would trigger the else statement
             System.out.println(beer + " bottles of beer on the wall. " + beer
                     + " bottles of beer. ");
             beer = beer - 1;
             if (beer == 1) {
-                System.out.println("Take one down, pass it around, " + beer + " bottle of beer on the wall.");
+                System.out.println("Take one down, pass it around, " + beer + " bottle of beer on the wall." + "\n");
                 System.out.println(beer + " bottle of beer on the wall. " + beer + " bottle of beer.");
                 beer = beer - 1;
-                System.out.println("Take one down, pass it around, " + beer + " bottles of beer on the wall.");
+                System.out.println("Take one down, pass it around, " + beer + " bottles of beer on the wall."  + "\n");
             } else {
-                System.out.println("Take one down, pass it around, " + beer + " bottles of beer on the wall.");
+                System.out.println("Take one down, pass it around, " + beer + " bottles of beer on the wall." + "\n");
             }
         }
         System.out.println(beer + " bottles of beer on the wall. " + beer + " bottles of beer.");
